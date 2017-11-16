11cd66f0ee00095e9fd5fa6cfba1052a99f01de2
danukaw <danuka.wijetunge@ebuilder.com>
2017-11-16 14:18:41 +0530

    eshint tutorial

---
+++ app.js
@@ -0,0 +1,18 @@
+const boo = 10;
+const obj = {
+  name: 'danuka',
+  age: 26
+};
+
+const value = new Promise((resolve, reject) => {
+  resolve('Some date');
+});
+
+value.then((data) => {
+  console.log(data);
+});
+
+const array = ['s', 'er'];
+
+document.write(obj);
+document.write(array);
